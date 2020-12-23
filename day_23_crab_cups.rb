ecc7b939f9b9f62e2db2305c82fd7323e7de6bf7
hunli <hunli.davis@gmail.com>
2020-12-23 11:41:03 -0800

    Update day_23_crab_cups.rb

--- day_23_crab_cups.rb
+++ day_23_crab_cups.rb
@@ -1,64 +1,143 @@
-def work(array, move_target = 100)
+class Node
+  attr_accessor :next, :prev, :value
+
+  def initialize(value)
+    @value = value
+  end
+end
+
+class LinkedList
+  attr_accessor :head, :tail, :hash
+
+  def initialize
+    @hash = {}
+  end
+
+  def add(value)
+    if @head
+      node = Node.new(value)
+      node.prev = @tail
+      @tail.next = node
+      @tail = node
+      @hash[value] = node
+    else
+      @head = Node.new(value)
+      @tail = @head
+      @hash[value] = @head
+    end
+  end
+end
+
+def next_three(node)
+  next_node = node.next
+
+  head_of_three = next_node
+  next_node = next_node.next
+  next_node = next_node.next
+
+  node.next = next_node.next
+  next_node.next.prev = node
+  next_node.next = nil
+  head_of_three.prev = nil
+
+  [node, head_of_three]
+end
+
+def include?(node, value)
+  until node == nil
+    return true if node.value == value
+    node = node.next
+  end
+
+  false
+end
+
+def get_destination_cup(current_node, next_three, max_size)
+  desired = current_node.value - 1
+
+  while include?(next_three, desired) || desired == 0
+    desired -= 1
+
+    if desired <= 0
+      desired = max_size
+    end
+  end
+
+  desired
+end
+
+def work(node, hash, max_size, move_target = 100)
   current_index = 0
   move = 1
 
   while move <= move_target
-    current_cup = array[current_index]
-    next_three = []
+    # p move
+    node, head_of_three = next_three(node)
 
-    3.times do |x|
-      next_three << array[(current_index + x + 1) % array.length]
-    end
+    destination_cup = get_destination_cup(node, head_of_three, max_size)
 
-    array.delete_if {|x| next_three.include?(x)}
-    destination_cup = current_cup - 1
+    destination_node = hash[destination_cup]
+    tail_of_three = head_of_three.next.next
 
-    index = (current_index + 1) % array.length
+    tail_of_three.next = destination_node.next
+    destination_node.prev = tail_of_three
+    destination_node.next = head_of_three
+    head_of_three.prev = destination_node
 
-    until array[index % array.length] == destination_cup
-      if destination_cup == 0
-        destination_cup = array.max
-      elsif next_three.include?(destination_cup)
-        destination_cup -= 1
-      end
-
-      index += 1
-    end
-
-    index = index % array.length
-    array.insert(index + 1, *next_three)
-
-    array =  array[current_index + 1..-1] + array[0..current_index]
-    current_index = 0
+    node = node.next
     move += 1
   end
 
-  array
+  node
 end
 
-def get_clockwise_labels(array)
-  start_index = array.index(1)
-  (array[start_index + 1..-1] + array[0...start_index]).map(&:to_s).join
+def get_clockwise_labels(node)
+  next_node = node.next
+  values = []
+
+  until next_node == node
+    values << next_node.value
+    next_node = next_node.next
+  end
+
+  values.map(&:to_s).join
+end
+
+def convert_to_nodes(input)
+  list = LinkedList.new
+  input.to_s.split("") { |value| list.add(value.to_i) }
+  list
 end
 
 def part1(input)
-  array = input.to_s.split("").map(&:to_i)
-  array = work(array)
-  get_clockwise_labels(array)
+  list = convert_to_nodes(input)
+
+  node = list.head
+  node.prev = list.tail
+  list.tail.next = node
+
+  hash = list.hash
+  work(node, hash, input.to_s.length)
+  get_clockwise_labels(hash[1])
 end
 
 def part2(input)
-  array = input.to_s.split("").map(&:to_i)
+  list = convert_to_nodes(input)
   iteration_num = 1_000_000
-  (10..iteration_num).each {|x| array << x}
+  (10..iteration_num).each {|x| list.add(x) }
 
-  array = work(array, iteration_num * 10)
-  start_index = array.index(1)
-  [array[start_index + 1], array[start_index + 2]]
+  node = list.head
+  node.prev = list.tail
+  list.tail.next = node
+
+  hash = list.hash
+  work(node, hash, iteration_num, iteration_num * 10)
+
+  hash[1].next.value * hash[1].next.next.value
 end
 
 input = 315679824
 dummy_input = 389125467
 
-p part1(input)
-p part2(input)
+p "Part 1: #{part1(input)}"
+p "Part 2: #{part2(input)}"
